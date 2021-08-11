897fd26fd8baf88fda479d69c25cc9f7ae0a38c1
ellijs <ellijs@hotmail.com>
2021-08-11 02:54:14 -0400

    Completed Assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+    attr_accessor :first, :second, :third
+
+    def initialize (first, second, third)
+      @first = first
+      @second = second
+      @third = third
+    end
+
+    def kind  
+      if ((@first > 0 && @second > 0 && @third > 0) && 
+      (
+      @first + @second > @third && 
+      @first + @third > @second && 
+      @second + @third > @first
+      )) 
+        if ( @first == @second && @second == @third )
+          :equilateral
+        elsif ( @first == @second || @first == @third || @second == third )
+          :isosceles 
+        else
+          :scalene 
+        end
+      else
+        begin 
+          raise TriangleError
+        end
+      end
+    end
+
+    class TriangleError < StandardError 
+    end
 end
